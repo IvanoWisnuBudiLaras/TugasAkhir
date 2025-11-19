64f37220c18b39b8529bd84d5caa2b1f9513575a
IvanoWisnuBudiLaras <ivanowisnubudilaras2008@gmail.com>
2025-11-19 13:01:41 +0700

    penambahan graphql untuk user

--- Smoethievibes/apps/backend/src/app.module.ts
+++ Smoethievibes/apps/backend/src/app.module.ts
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿import { Module } from "@nestjs/common";
+﻿﻿﻿﻿﻿﻿import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UserModule } from "./modules/user/user.module";
